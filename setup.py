9c4c65e5dc4f429a6f72464d3b875bdbe75aaa7b
Pablo Moreno <pablo.a.moreno@gmail.com>
2016-01-28 13:38:38 +0000

    Adds dependency declaration on setup.

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(
     author='Pablo Moreno, Pierrick Roger Mele',
     author_email='phenomenal-h2020-users@googlegroups.com',
     scripts=['submit_k8s_job'],
+    install_requires=[
+          'PyYAML',
+      ],
     description='CLI to send and wait for jobs to a Kubernetes cluster, '
                 'requires a working kubectl (can connect to the desired K8s '
                 'cluster) on the machine running this code.'
